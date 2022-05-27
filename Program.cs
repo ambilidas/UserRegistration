add7fc9ab608f4a589354ac6c92e0e0c9bb03dac
Ambili das v s <ambili.kan@gmail.com>
2022-05-27 13:39:25 +0530

    Program for user registration using REG_EX

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\t\tWelcome to User Registration Program");
+            Console.ReadKey();
+        }
+    }
+}
